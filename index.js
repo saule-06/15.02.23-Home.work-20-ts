0090dea7dd923b0ae21c1cf461c9a4cfaaa237bc
Saule Toigonbaeva <tojgonbaevasaule@gmail.com>
2023-02-17 11:25:22 +0600

    structure for finding the volume of a cone

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function myName(name, surname) {
   console.log("My name is " + name + surname);
 }
 myName("Saule ", " Toigonbaeva");
+
+// #2
+function coneVolume(height, radius) {
+  cVolume =  ((height * 3.14 * (radius) ** 2) / 3);
+  console.log(cVolume);
+}
